81e3df72286d08e6663edf1a6ed4933cc6c122cb
Guilherme Cardoso <guilherme.martins@itixti.com.br>
2023-10-13 13:56:12 -0300

    mudando a rota

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 app.use(cors({
-  origin: 'https://card-visit.vercel.app/v1/send-email', // Permita solicitações do endereço Angular
+  origin: 'https://cartaodevisitas-28cd2.web.app', // Permita solicitações do endereço Angular
   methods: 'POST',
   credentials: true, // Permita que os cabeçalhos de autenticação sejam enviados
 }));
