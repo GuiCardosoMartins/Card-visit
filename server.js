1f3e7a9f05053e2e23e2777a6d2edf0165dbd416
Guilherme Cardoso <guilherme.martins@itixti.com.br>
2023-10-13 11:56:03 -0300

    rota adicionada

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 const app = express();
 app.use(cors({
-  origin: 'https://card-visit.vercel.app/', // Permita solicitações do endereço Angular
+  origin: 'https://card-visit.vercel.app/v1/send-email', // Permita solicitações do endereço Angular
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   credentials: true, // Permita que os cabeçalhos de autenticação sejam enviados
 }));
